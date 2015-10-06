574eb09a7704107bbea4115c6d8fb4554fb8be7f
Josh McKelvey <jmckelvey0@gmail.com>
2015-10-05 22:05:07 -0400

    whoops first commit including index view and add, edit, destroy links

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Course.create!(name: "Appetizer")
+Course.create!(name: "Side")
+Course.create!(name: "Entree")
+Course.create!(name: "Dessert")
